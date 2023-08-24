07bd7c0cd9fb70816acfc657a59764daa84632f3
Aziz <74332587+oh-az@users.noreply.github.com>
2023-08-24 16:02:17 +0300

    Update HashMorpher.cpp

--- HashMorpher.cpp
+++ HashMorpher.cpp
@@ -13,17 +13,17 @@
 
 bool copyFile(const char* sourcePath, const char* destinationPath) {
     if (!CopyFileA(sourcePath, destinationPath, FALSE)) {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to copy file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         return false;
     }
 
     HANDLE destinationFile = CreateFileA(destinationPath, FILE_APPEND_DATA, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (destinationFile == INVALID_HANDLE_VALUE) {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to open destination file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         return false;
     }
 
@@ -34,9 +34,9 @@ bool copyFile(const char* sourcePath, const char* destinationPath) {
     DWORD bytesWritten;
     if (!WriteFile(destinationFile, &byte1, sizeof(byte1), &bytesWritten, NULL) ||
         !WriteFile(destinationFile, &byte2, sizeof(byte2), &bytesWritten, NULL)) {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to write bytes to file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         CloseHandle(destinationFile);
         return false;
     }
@@ -49,26 +49,26 @@ bool copyFile(const char* sourcePath, const char* destinationPath) {
 void printFileHash(const char* filePath) {
     HANDLE fileHandle = CreateFileA(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (fileHandle == INVALID_HANDLE_VALUE) {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to open file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         return;
     }
 
     HCRYPTPROV hCryptProv;
     if (!CryptAcquireContext(&hCryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to acquire cryptographic context. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         CloseHandle(fileHandle);
         return;
     }
 
     HCRYPTHASH hHash;
     if (!CryptCreateHash(hCryptProv, CALG_MD5, 0, 0, &hHash)) {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to create hash object. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+       //printf(ANSI_COLOR_RESET);
         CryptReleaseContext(hCryptProv, 0);
         CloseHandle(fileHandle);
         return;
@@ -79,9 +79,9 @@ void printFileHash(const char* filePath) {
     DWORD bytesRead;
     while (ReadFile(fileHandle, buffer, bufferSize, &bytesRead, NULL) && bytesRead > 0) {
         if (!CryptHashData(hHash, buffer, bytesRead, 0)) {
-            printf(ANSI_COLOR_RED);
+            //printf(ANSI_COLOR_RED);
             printf("Failed to hash data. Error code: %d\n", GetLastError());
-            printf(ANSI_COLOR_RESET);
+            //printf(ANSI_COLOR_RESET);
             CryptDestroyHash(hHash);
             CryptReleaseContext(hCryptProv, 0);
             CloseHandle(fileHandle);
@@ -99,9 +99,9 @@ void printFileHash(const char* filePath) {
         printf("\n");
     }
     else {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Failed to retrieve hash value. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
     }
 
     CryptDestroyHash(hHash);
@@ -115,24 +115,24 @@ int main() {
     char destinationPath[MAX_PATH] = "";
 
     // ASCII art
-    printf(ANSI_COLOR_RED);
+    //printf(ANSI_COLOR_RED);
     printf("\t\t\t|_| _  _|_ |\\/| _  _ _ |_  _  _\n");
     printf("\t\t\t| |(_|_\\| ||  |(_)| |_)| |(/_| \n");
     printf("\t\t\t                    |           \n");
-    printf(ANSI_COLOR_RESET);
-    printf(ANSI_COLOR_BLUE);
+    //printf(ANSI_COLOR_RESET);
+    //printf(ANSI_COLOR_BLUE);
     printf("\t\t\t\t\tBy Abdulaziz Almetairy\n");
     printf("\t\t\t\t\t   github.com/oh-az\n\n\n");
-    printf(ANSI_COLOR_RESET);
+    //printf(ANSI_COLOR_RESET);
 
-    printf(ANSI_COLOR_GREEN);
+    //printf(ANSI_COLOR_GREEN);
     printf("Select an option:\n\n");
     printf("1: CMD\n");
     printf("2: Powershell\n");
     printf("3: Powershell_ise\n");
     printf("4: Custom path\n\n");
     printf("#  ");
-    printf(ANSI_COLOR_RESET);
+    //printf(ANSI_COLOR_RESET);
     scanf_s("%d", &choice);
 
     switch (choice) {
@@ -146,22 +146,22 @@ int main() {
         sourcePath = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\PowerShell_ise.exe";
         break;
     case 4:
-        printf(ANSI_COLOR_GREEN);
+       //printf(ANSI_COLOR_GREEN);
         printf("Enter custom source path: ");
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         char customSourcePath[MAX_PATH];
         scanf_s("%s", customSourcePath, sizeof(customSourcePath));
         sourcePath = customSourcePath;
         break;
     default:
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("Invalid choice.\n");
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
         return 1;
     }
-    printf(ANSI_COLOR_GREEN);
+    //printf(ANSI_COLOR_GREEN);
     printf("Enter destination path (leave empty for current directory): ");
-    printf(ANSI_COLOR_RESET);
+    //printf(ANSI_COLOR_RESET);
     getchar();  // Consume the newline character after choice input
 
     char input[MAX_PATH];
@@ -175,9 +175,9 @@ int main() {
         // If the destination path is empty, use the current directory with the copied file name appended with "_new"
         const char* fileName = strrchr(sourcePath, '\\');
         if (fileName == NULL) {
-            printf(ANSI_COLOR_RED);
+            //printf(ANSI_COLOR_RED);
             printf("Invalid source path.\n");
-            printf(ANSI_COLOR_RESET);
+            //printf(ANSI_COLOR_RESET);
             return 1;
         }
         fileName++;  // Move past the '\' character
@@ -196,28 +196,28 @@ int main() {
     if (copyFile(sourcePath, destinationPath)) {
         char fullPath[MAX_PATH];
         if (GetFullPathNameA(destinationPath, sizeof(fullPath), fullPath, NULL) == 0) {
-            printf(ANSI_COLOR_RED);
+            //printf(ANSI_COLOR_RED);
             printf("Failed to get full path of the destination file. Error code: %d\n", GetLastError());
-            printf(ANSI_COLOR_RESET);
+            //printf(ANSI_COLOR_RESET);
         }
         else {
-            printf(ANSI_COLOR_GREEN);
+            //printf(ANSI_COLOR_GREEN);
             printf("\nFile copied successfully to: %s\n\n", fullPath);
-            printf(ANSI_COLOR_MAGENTA);
+            //printf(ANSI_COLOR_MAGENTA);
             printf("Calculating hash values...\n\n");
-            printf(ANSI_COLOR_YELLOW);
+            //printf(ANSI_COLOR_YELLOW);
             printf("Old File Hash:\n");
             printFileHash(sourcePath);
-            printf(ANSI_COLOR_BLUE);
+            //printf(ANSI_COLOR_BLUE);
             printf("\nNew File Hash:\n");
             printFileHash(destinationPath);
-            printf(ANSI_COLOR_RESET);
+            //printf(ANSI_COLOR_RESET);
         }
     }
     else {
-        printf(ANSI_COLOR_RED);
+        //printf(ANSI_COLOR_RED);
         printf("File copy failed.\n");
-        printf(ANSI_COLOR_RESET);
+        //printf(ANSI_COLOR_RESET);
     }
 
     return 0;
